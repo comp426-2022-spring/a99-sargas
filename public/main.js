8b10a5b21cd9642cf2865f2613ed4801c5bd663b
Albert "Christian" DeMarco <Kelter-1313>
2022-04-30 17:58:17 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,26 +4,28 @@ loginButton.addEventListener("submit", loginHandler)
 // Create the submit handler
 async function loginHandler(event) {
     event.preventDefault();
-    const endpoint = "app/login";
-    const url = document.baseURI + endpoint;
+    var endpoint = "app/login";
+    var url = document.baseURI + endpoint;
     const formEvent = event.currentTarget
     console.log(formEvent)
    // try{
-        const formData = new FormData(formEvent);
-        console.log(formData)
-        const loginResult = await sendLogin({formData, url})
-        console.log(loginResult)
-        document.getElementById("loginStatusCheck").innerHTML=loginResult
+    const formData = new FormData(formEvent);
+    console.log(formData)
+    const loginResult = await sendingStuff({formData, url})
+    console.log(loginResult)
+    document.getElementById("loginStatusCheck").innerHTML=loginResult
+    if(loginResult == "LOGIN"){
+        endpoint = "app/login"; // replace with name for endpoint grabbing the entries of the person
+        url = document.baseURI + endpoint;
+        sendingStuff({formData, url})
+    }
+
   //  }catch(e){
   //      console.error("oof")
   //  }
 }
 
-
-
-
-
-async function sendLogin({ formData, url}) {
+async function sendingStuff({ formData, url}) {
     const plainFormData = Object.fromEntries(formData.entries());
     console.log("plainData")
     console.log(plainFormData)
