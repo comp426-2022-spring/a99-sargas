e09a1863b00af0a060a6a8c183715567c0dae1c7
Albert "Christian" DeMarco <Kelter-1313>
2022-04-30 12:32:31 -0400

    Revert "focusing on login"
    This reverts commit 1226823dbd97b51c583a9c92075173a978f6ac7d.

--- database/user.js
+++ database/user.js
@@ -7,9 +7,7 @@ const db = new Database('user.db');
 
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';
-    SELECT name FROm sqlite_mater WHERE type`
-    
+    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
     );
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
@@ -19,10 +17,9 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT ); 
+        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT );
         CREATE TABLE feelinginfo (id INTEGER PRIMARY KEY, username TEXT, feeling TEXT, date VARCHAR);
-    `; // removed email from userInfo... If we try to use it later, well, then that is the issue
+    `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
 // Echo information about what we just did to the console.
--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 const app = express()
 const args = require('minimist')(process.argv.slice(2)) 
@@ -99,4 +100,86 @@ app.get("/app/graph/:id", (req, res) => {
         console.error(e)
     }
 
+=======
+const express = require('express')
+const app = express()
+const args = require('minimist')(process.argv.slice(2)) 
+const morgan = require('morgan')
+const database = require('./src/services/database.js')
+const fs = require('fs')
+const md5 = require('md5')
+
+// Serve static HTML files
+app.use(express.static('./public'));
+
+// Add cors dependency
+const cors = require('cors')
+// Set up cors middleware on all endpoints
+app.use(cors())
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%',port))
+})
+
+app.get('/app/', (req, res) => {
+    //Respond w status 200
+    res.statusCode = 200;
+    //Respond w status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, {
+        'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' + res.statusMessage)
+});
+
+app.get('/app/moreInfo', (req, res) => {
+    //Respond w status 200
+    res.statusCode = 200;
+    //Respond w status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, {
+        'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' + res.statusMessage)
+});
+
+app.get('/app/login', (req, res) => {
+    //Respond w status 200
+    res.statusCode = 200;
+    //Respond w status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, {
+        'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' + res.statusMessage)
+});
+
+app.use(function(req, res) {
+    res.type("text/plain")
+    res.status(404).send("Endpoint does not exist")
+  
+
+})
+
+app.post("/app/feeling/:user", (req, res, next) => {
+    let data = {
+        user: req.body.username,
+        pass: new Date()
+    }
+    //need to get user from  other parts
+
+    const stmt = db.prepare('INSERT INTO feelinginfo (username, feeling, date) VALUES (?, ?)')
+    const info = stmt.run(data.user, data.pass)
+    res.status(200).json(info)
+});
+
+app.get("/app/graph/:id", (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ?').get(req.params.id);
+        res.status(200).json(stmt)
+    } catch (e) {
+        console.error(e)
+    }
+
+>>>>>>> parent of 1226823 (focusing on login)
 });
