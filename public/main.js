49711de1171c2909aff5a3fb3e19907b3331bbf1
Albert "Christian" DeMarco <Kelter-1313>
2022-04-30 18:32:18 -0400

    registerWorking
    Got register form to work ; have tested login on the registered pair immediately after!

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const loginButton = document.getElementById("loginForm")
 // Add event listener for coins form
+const registerButton = document.getElementById("registerForm")
+registerButton.addEventListener("submit", registerHandler)
 loginButton.addEventListener("submit", loginHandler)
 // Create the submit handler
 async function loginHandler(event) {
@@ -13,16 +15,33 @@ async function loginHandler(event) {
     console.log(formData)
     const loginResult = await sendingStuff({formData, url})
     console.log(loginResult)
-    document.getElementById("loginStatusCheck").innerHTML=loginResult
     if(loginResult == "LOGIN"){
+        document.getElementById("loginStatusCheck").innerHTML="Logged in!!! Look below for your entries!"
         endpoint = "app/login"; // replace with name for endpoint grabbing the entries of the person
         url = document.baseURI + endpoint;
         sendingStuff({formData, url})
+    }else if(loginResult == "incorrectPassword"){
+        document.getElementById("loginStatusCheck").innerHTML="Wrong password..."
+    }else if(loginResult == "badUsername"){
+        document.getElementById("loginStatusCheck").innerHTML="We don't recognize that username... Consider making an account below!"
     }
 
   //  }catch(e){
   //      console.error("oof")
   //  }
+}
+
+async function registerHandler(event){
+    event.preventDefault();
+    var endpoint = "app/register";
+    var url = document.baseURI + endpoint;
+    const formEvent = event.currentTarget
+  //  console.log(formEvent)
+   // try{
+    const formData = new FormData(formEvent);
+  //  console.log(formData)
+    const loginResult = await sendingStuff({formData, url})
+    console.log(loginResult)
 }
 
 async function sendingStuff({ formData, url}) {
